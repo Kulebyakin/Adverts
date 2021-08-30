7af819f41d69e717bc9f54858b1f9bd5f156d09a
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-08-30 23:02:21 +0800

    Show only published adverts on category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,22 +4,12 @@ class CategoriesController < ApplicationController
     @parent_categories = Category.where(parent_category: nil)
 
     if @category.parent_category == nil
-      @adverts = Advert.joins(:categories).where(categories: @category.subcategories)
+      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category.subcategories)
       @subcategories = @category.subcategories
     else
-      @adverts = Advert.joins(:categories).where(categories: @category)
+      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category)
       @subcategories = @category.parent_category.subcategories
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_category
-    #   @category = Category.find(params[:id])
-    # end
-
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:title, :id)
-    end
 end
