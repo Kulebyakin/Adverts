e0db65b75124fb1f7996ba9c6ea69b83ad99b7d6
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-01 20:01:07 +0800

    Add pagination to category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,10 +4,10 @@ class CategoriesController < ApplicationController
     @parent_categories = Category.where(parent_category: nil)
 
     if @category.parent_category == nil
-      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category.subcategories)
+      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category.subcategories).page(params[:page])
       @subcategories = @category.subcategories
     else
-      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category)
+      @adverts = Advert.where(status: :published).joins(:categories).where(categories: @category).page(params[:page])
       @subcategories = @category.parent_category.subcategories
     end
   end
